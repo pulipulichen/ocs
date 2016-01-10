5794b7cc5c91c37b41f542ecbcbd3f4c1cdb5da0
Pulipuli Chen <pulipuli.chen@gmail.com>
2016-01-10 22:42:10 +0800

    0110 好，上傳檔案功能完成

--- lib/pkp/lib/tinymce/jscripts/tiny_mce/plugins/jbimages/ci/application/language/chinese/jbstrings_lang.php
+++ lib/pkp/lib/tinymce/jscripts/tiny_mce/plugins/jbimages/ci/application/language/chinese/jbstrings_lang.php
@@ -1,8 +1,8 @@
 <?php
 
 $lang['jb_blankpage_message'] = "The Upload process had not started, or started, but had not finished yet, or your browser could not reach the remote server.";
-$lang['jb_delete'] = "刪除";
-$lang['jb_insert'] = "插入";
+$lang['jb_delete'] = "Delete";
+$lang['jb_insert'] = "Insert";
 $lang['jb_no_files'] = "You have not uploaded any files";
 $lang['jb_go_back'] = "Go back";
 $lang['jb_list_disabled'] = "This server has disabled file listing for uploaded images";
--- lib/pkp/lib/tinymce/jscripts/tiny_mce/plugins/jbimages/ci/application/views/file_list.php
+++ lib/pkp/lib/tinymce/jscripts/tiny_mce/plugins/jbimages/ci/application/views/file_list.php
@@ -1,19 +1,67 @@
+<html>
 <head>
 	<title><?php echo lang('jb_uploaded_images') ?></title>
 	<link href="../../../css/dialog.css" rel="stylesheet" type="text/css" />
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<script type="text/javascript" src="../../../../../tiny_mce_popup.js"></script>
 </head>
-
+<body>
 <div id="imageListing">
+    
 <?php if($files) {
-		foreach ($files as $file) { ?>
+    ?>
+    <ul>
+        <?php
+    $img_ext = array("git", "jpg", "jpeg", "png");
+		foreach ($files as $file) { 
+                        echo "<li>";
+                    $img_path = $file['img_path'];
+                    $ext = substr($img_path, strrpos($img_path, ".")+1);
+                    //echo $ext;
+                    ?>
+                    <a href="javascript:insertFile('<?php echo html_escape($file['img_path']) ?>')">
+                    <?php
+                    if (in_array($ext, $img_ext)) {
+                    ?>
 			<img src="<?php echo html_escape($file['img_path']) ?>" width="25" height="25" alt="<?php echo html_escape($file['name']) ?>" />
-			<a href="<?php echo html_escape($file['img_path']) ?>"><?php echo html_escape($file['name']) ?></a>&nbsp;
+                    <?php
+                    }
+                    ?>
+			<?php echo html_escape($file['name']) ?></a>&nbsp;
 			(<?php echo html_escape($file['size']) ?> kB)&nbsp;&nbsp;&nbsp;
-			<span class="delete"><a href="../deleteImage/<?php echo urlencode($file['name']) ?>"><?php echo lang('jb_delete') ?></a></span>
-			<br />
-<?php 		}
+                        <div>
+                            <span class="delete"><a href="../deleteImage/<?php echo urlencode($file['name']) ?>"><?php echo lang('jb_delete') ?></a></span>
+                        </div>
+                        </li>
+                    <?php
+                    }
+                    ?>
+        </ul>
+    <?php
 	} else { ?>
 	<h3><?php echo lang('jb_no_files') ?></h3>
 	<a href="../../../dialog.htm"><?php echo lang('jb_go_back') ?></a>
 <?php } ?>
+    
 </div>
+<script type="text/javascript">
+    function insertFile(_path) {
+        //alert(_path);
+        console.log(window.opener.tinyMCEPopup)
+                        var _filename = _path;
+                        var _f_name = _filename.substring(_filename.lastIndexOf("/")+1, _filename.length);
+                        var _ext = _filename.substring(_filename.lastIndexOf(".")+1, _filename.length);
+                        _ext = _ext.toLowerCase();
+                        var _img_ext = ["gif", "jpg", "jpeg", "png"];
+                        //alert([_f_name, _ext, typeof($)]);
+                        if (_img_ext.indexOf(_ext) > -1) { 
+                            tinyMCEPopup.editor.execCommand('mceInsertContent', false, '<img src="' + _filename +'" alt="" />');
+                        }
+                        else {
+                            tinyMCEPopup.editor.execCommand('mceInsertContent', false, '<a href="' + _filename +'" target="_blank">'+_f_name+'</a>');
+                        }
+			tinyMCEPopup.close();
+    }
+</script>
+</body>
+</html>
