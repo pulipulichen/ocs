4195388fc2298e99cbcc76376f0ea8ebbdeaa5bb
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-05-18 20:00:57 +0800

    西元2024年05月18日 (週六) 20時00分57秒 CST

--- build/tag.sh
+++ build/tag.sh
@@ -34,9 +34,10 @@ echo "Steps:"
 echo "1. Zenono GitHub Setting:  https://zenodo.org/account/settings/github/"
 echo "2. GitHub New Release:  https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/new"
 echo "3. Zenono Repository Management:  https://zenodo.org/account/settings/github/repository/${GITHUB_USER}/${GITHUB_REPO}"
-echo "4. GitHub Homepage:  ${GITHUB_HOMEPAGE}"
+echo "4. GitHub New Release Again:  https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/new"
+echo "5. GitHub Homepage:  ${GITHUB_HOMEPAGE}"
 
 if [ -n "$DOI" ]; then
-  echo "5. Zenono Public:  https://doi.org/${DOI}"
+  echo "6. Zenono Public:  https://doi.org/${DOI}"
 fi
 echo "================================================"
