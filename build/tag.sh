45857f4af1ad5a58cb5848412481998c3be3b376
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-05-18 20:16:03 +0800

    西元2024年05月18日 (週六) 20時16分03秒 CST

--- build/tag.sh
+++ build/tag.sh
@@ -34,10 +34,10 @@ echo "Steps:"
 echo "1. Zenono GitHub Setting:  https://zenodo.org/account/settings/github/"
 echo "2. GitHub New Release:  https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/new"
 echo "3. Zenono Repository Management:  https://zenodo.org/account/settings/github/repository/${GITHUB_USER}/${GITHUB_REPO}"
-echo "4. GitHub New Release Again:  https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/new"
-echo "5. GitHub Homepage:  ${GITHUB_HOMEPAGE}"
-
+echo "4. Create new tag and push to GitHub."
 if [ -n "$DOI" ]; then
-  echo "6. Zenono Public:  https://doi.org/${DOI}"
+  echo "5. GitHub New Release Again:  https://github.com/${GITHUB_USER}/${GITHUB_REPO}/releases/new"
+  echo "6. GitHub Homepage:  ${GITHUB_HOMEPAGE}"
+  echo "7. Zenono Public:  https://doi.org/${DOI}"
 fi
 echo "================================================"
